d65eb4778aaef77ff09b9c1c969bf3ed3d114536
rijulsoni <70277438+rijulsoni@users.noreply.github.com>
2024-07-21 22:52:58 +0530

    fixed skills section responsiveness

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -8,7 +8,7 @@ export default function Skills() {
   return (
     <div
       className={`flex flex-col items-center ${
-        value.isDarkMode ? "bg-[#1C1C27]" : "bg-slate-100"
+        value.isDarkMode ? "bg-[#0d0d1a]" : "bg-slate-100"
       } `}
       style={{ fontFamily: "poppins" }}
     >
@@ -27,25 +27,25 @@ export default function Skills() {
         Here are some of my skills on which I have been working on for the past
         2 years.
       </div>
-      <div className="w-[500px] min-[1083px]:w-[1000px] mt-8 grid grid-cols-1 gap-8 min-[1083px]:grid-cols-2">
+      <div className="w-[300px] sm:w-[400px] lg:[500px] min-[1083px]:w-[1000px] mt-8 grid grid-cols-1 gap-8 min-[1083px]:grid-cols-2">
         {skills.map((skill) => (
           <div
-            className={`rounded-3xl shadow-sm border-2 border-[#854CE6] mb-10 ${
+            className={`rounded-3xl shadow-sm border-2 border-[#854CE6]  mb-1 sm:mb-10 ${
               value.isDarkMode ? "bg-[#1C1C27]" : "bg-slate-100"
             } ${value.isDarkMode ? "text-[#F2F3F495]" : "text-[#11111195]"}`}
             key={skill.id}
           >
             <div
-              className={`mb-5 text-center text-3xl font-medium px-5 py-7 ${
+              className={`-mb-3 sm:mb-5 text-center text-2xl sm:text-3xl font-medium px-5 py-5 sm:py-7 ${
                 value.isDarkMode ? "text-[#F2F3F495]" : "text-black"
               }`}
             >
               {skill.title}
             </div>
-            <div className="flex justify-center flex-wrap gap-5 mb-20 ">
+            <div className="flex justify-center flex-wrap gap-2 sm:gap-6 mb-8 sm:mb-20 ">
               {skill.skills.map((item) => (
                 <div
-                  className="text-xl font-normal flex items-center justify-center "
+                  className="text-base lg:text-xl font-normal flex items-center justify-center "
                   key={item.id}
                 >
                   <div
@@ -53,7 +53,7 @@ export default function Skills() {
                       value.isDarkMode
                         ? "border-[#F2F3F480]"
                         : "border-[#11111180]"
-                    } rounded-2xl px-3 py-4 flex gap-3`}
+                    } rounded-2xl px-2 py-4 flex gap-3`}
                   >
                     <img alt="skill name" width="24px" height="24px" src={item.image} />
                     {item.name}
